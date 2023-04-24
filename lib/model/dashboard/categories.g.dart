f59477069ce2b3d92f58f37ffb410a2b44a8d513
Purna chandra <purnachandra.a@westagilelabs.com>
2023-04-24 18:51:42 +0530

    ui changes

---
+++ lib/model/dashboard/categories.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'categories.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Categories _$CategoriesFromJson(Map<String, dynamic> json) => Categories(
+      isClick: json['is_click'] as bool?,
+      title: json['title'] as String?,
+    );
+
+Map<String, dynamic> _$CategoriesToJson(Categories instance) =>
+    <String, dynamic>{
+      'is_click': instance.isClick,
+      'title': instance.title,
+    };
---
+++ lib/model/dashboard/menu.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'menu.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Menu _$MenuFromJson(Map<String, dynamic> json) => Menu(
+      icon: json['icon'] as String?,
+      title: json['title'] as String?,
+    );
+
+Map<String, dynamic> _$MenuToJson(Menu instance) => <String, dynamic>{
+      'icon': instance.icon,
+      'title': instance.title,
+    };
