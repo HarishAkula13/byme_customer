cd758b7b1ca8630b6cc5f5d3dccc1cc774e81275
Purna chandra <purnachandra.a@westagilelabs.com>
2023-04-24 14:23:02 +0530

    ui changes

--- lib/common/utilities/fonts.dart
+++ lib/common/utilities/fonts.dart
@@ -1,6 +1,6 @@
 
 abstract class Inter {
-  static const regular = 'InterRegular';
+  static const regular = 'InterMedium';
   static const bold = 'InterBold';
   static const semiBold = 'InterSemiBOld';
   static const medium = 'InterMedium';
