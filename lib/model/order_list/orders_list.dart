eb0dabe0a614754c3f8f981dd12a4b8fbde583cb
Akula  RaoPurnachanadra Rao <a.chandu006@gmail.com>
2024-05-03 19:17:34 +0530

    Added icons

--- lib/model/order_list/orders_list.dart
+++ lib/model/order_list/orders_list.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 part 'orders_list.g.dart';
 @JsonSerializable(fieldRename: FieldRename.snake)
 class OrdersList {
-  dynamic orderList;
+  List<OrdersList>? orderList;
   String? orderType;
   String? orderId;
   String? orderDateTime;
